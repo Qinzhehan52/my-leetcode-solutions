0cc5ca708065e7ddbbb80f3107a3c4a0f3129498
qinzhehan <zhehanqin@sf-express.com>
2019-08-29 10:43:37 +0800

    1026 Maximum Difference Between Node and Ancestor

---
+++ cpp/src/1026.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode *root) {
+        return dfs(root, root->val, root->val);
+    }
+
+    int dfs(TreeNode* root, int min, int max)
+    {
+        if (root->val > max) max = root->val;
+        if (root->val < min) min = root->val;
+
+        int left = 0, right = 0;
+        if (root->left || root->right) {
+            if (root->left) left = dfs(root->left, min, max);
+            if (root->right) right = dfs(root->right, min, max);
+
+            return left > right ? left : right;
+        }
+        return max - min;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode *stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode *root = new TreeNode(stoi(item));
+    queue<TreeNode *> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode *node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode *root = stringToTreeNode(line);
+
+        int ret = Solution().maxAncestorDiff(root);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
